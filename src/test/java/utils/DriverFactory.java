43753465e5c9e1a0cb813052ccdb8f61a522d361
Rajveer Parmar <rajveer.parmar@etatvasoft.com>
2025-07-30 11:47:26 +0530

    added implicit wait

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,6 +21,8 @@ public class DriverFactory {
             options.addArguments("--remote-allow-origins=*");
 
             driver = new ChromeDriver(options);
+            
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver;
     }
